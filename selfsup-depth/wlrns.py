f213b192a1a7ed9140e3b2017ced5503ba1d9024
nenad <nenad.markus@protonmail.com>
2022-06-13 16:19:09 +0200

    SKAR/wlrn loss

--- selfsup-depth/wlrns.py
+++ selfsup-depth/wlrns.py
@@ -62,12 +62,10 @@ def compute_matrix_entropy_loss(triplet, temp=20):
 	# similarity and probability matrices
 	S = torch.matmul(a, p.t())
 	P = torch.softmax(temp*S, dim=1)
-	#print(P[0].max().item(), P[0].min().item(), P[0].mean().item())
 	# compute the average entropy (per row)
 	H = - torch.mul(P, torch.log(P))
 	H = H.sum() / a.shape[0]
 	# we want to minimize entropy (i.e., we want the distribution to be spiky)
-	#print(H.item())
 	return H
 
 def loss_forward(featuremaps):
@@ -86,7 +84,8 @@ def loss_forward(featuremaps):
 		#
 		n = torch.cat([descs0[r-3], descs0[r+3], descs1[r-3], descs1[r+3]])
 		#
-		losslist.append( compute_matrix_entropy_loss((a, p, n))/10 + wlrn_loss_forward((a, p, n)) )
+		#losslist.append( compute_matrix_entropy_loss((a, p, n))/10 + wlrn_loss_forward((a, p, n)) )
+		losslist.append( wlrn_loss_forward((a, p, n)) )
 
 	return sum(losslist)/len(losslist)
 
