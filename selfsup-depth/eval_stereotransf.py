6c5f71778b3a65c6f049c05029d88a4002369df2
nenad <nenad.markus@protonmail.com>
2022-10-06 12:48:27 +0200

    StereoTransformer

--- selfsup-depth/eval_stereotransf.py
+++ selfsup-depth/eval_stereotransf.py
@@ -124,7 +124,7 @@ def compute_kitti_result_for_image_pair(_calc_disparity, folder, name, show=True
 		).float()
 	#disp[:, 0:200] = 0
 	#
-	disp_calculated = _calc_disparity(img0, img1)
+	disp_calculated = _calc_disparity(img0, img1).cpu().numpy()
 
 	# for gound truth: "A 0 value indicates an invalid pixel (ie, no ground truth exists, or the estimation algorithm didn't produce an estimate for that pixel)"
 	# for predicted: we can doscard some values based on low matching threshold
