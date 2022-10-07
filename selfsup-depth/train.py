cedf3b1d9b9e852dbfadb52303e5477e8c565f6c
nenad <nenad.markus@protonmail.com>
2022-10-07 15:13:33 +0200

    matrix entropy loss - revisited

--- selfsup-depth/train.py
+++ selfsup-depth/train.py
@@ -50,7 +50,7 @@ def compute_matrix_entropy_loss(ammpt, temp=20):
 	# similarity and probability matrices
 	S = ammpt
 	P = torch.softmax(temp*S, dim=1)
-	cv2.imwrite("P_.png", (255*P.detach()).byte().cpu().numpy())
+	if numpy.random.random()<0.1: cv2.imwrite("P_.png", (255*P.detach()).byte().cpu().numpy())
 	# compute the average entropy (per row)
 	H = - torch.mul(P, torch.log(P))
 	H = H.sum() / S.shape[0]
@@ -95,7 +95,8 @@ def loss_forward(left_features, right_features, threshold=0.8):
 		p = descs1[r]
 		n = torch.cat([descs0[r-3], descs0[r+3], descs1[r-3], descs1[r+3]])
 		# accumulate the loss
-		losslist.append( compute_triplet_loss((a, p, n), threshold) )
+		#losslist.append( compute_triplet_loss((a, p, n), threshold) )
+		losslist.append( compute_matrix_entropy_loss( torch.mm(a, p.t()) )
 
 	# we're done: average the loss
 	return sum(losslist)/len(losslist)
