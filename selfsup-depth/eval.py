4b34d912f0108d59d2d8cdd1e44658efa4a61102
nenad <nenad.markus@protonmail.com>
2022-10-13 11:03:03 +0200

    improve eval.py

--- selfsup-depth/eval.py
+++ selfsup-depth/eval.py
@@ -79,7 +79,9 @@ def calc_disparity(model, img0, img1, max_disp=96, filtering=None):
 	#
 	sims, disps = torch.max(scores, 2)
 	disps = disps.cpu().byte()
-	if filtering == "threshold":
+	if filtering == "none":
+		pass
+	elif filtering == "threshold":
 		disps[ sims < 0.75 ] = 0
 	elif filtering == "median":
 		disps = cv2.medianBlur(disps.numpy(), 17)
@@ -111,7 +113,7 @@ def get_bad_pixels(disp, disp_gt, valid_mask):
 
 	return outlier_mask, color_mask
 
-def compute_kitti_result_for_image_pair(_calc_disparity, folder, name, show=True):
+def compute_kitti_result_for_image_pair(_calc_disparity, folder, name, show=True, vizdir="vizdir", ignore_calc_zeros=False):
 	#
 	if usecolor:
 		img0 = torch.from_numpy(cv2.imread(folder+'/image_2/'+name, cv2.IMREAD_COLOR)).permute(2, 0, 1).float().div(255.0)
@@ -133,19 +135,27 @@ def compute_kitti_result_for_image_pair(_calc_disparity, folder, name, show=True
 
 	# for gound truth: "A 0 value indicates an invalid pixel (ie, no ground truth exists, or the estimation algorithm didn't produce an estimate for that pixel)"
 	# for predicted: we can doscard some values based on low matching threshold
-	valid_mask = (disp > 0) & (disp_calculated > 0)
+	if ignore_calc_zeros:
+		valid_mask = (disp > 0) & (disp_calculated > 0)
+	else:
+		valid_mask = (disp > 0)
 	outlier_mask, color_mask = get_bad_pixels(disp_calculated, disp, valid_mask)
 
-	if show:
+	if vizdir is not None:
+		os.system("mkdir -p '%s'" % vizdir)
 		left = cv2.resize(cv2.imread(folder+'/image_2/'+name, cv2.IMREAD_COLOR), None, fx=0.5, fy=0.5)
 		right = cv2.resize(cv2.imread(folder+'/image_3/'+name, cv2.IMREAD_COLOR), None, fx=0.5, fy=0.5)
-		viz = np.zeros((left.shape[0]+disp.shape[0]+disp_calculated.shape[0], disp.shape[1], 3), dtype=np.uint8)
+		cm = color_mask.permute(1, 2, 0).numpy()
+		viz = np.zeros((left.shape[0]+disp.shape[0]+disp_calculated.shape[0]+cm.shape[0], disp.shape[1], 3), dtype=np.uint8)
 		viz[:left.shape[0], :left.shape[1], :] = left
-		viz[:right.shape[0], left.shape[1]:, :] = right
+		viz[:right.shape[0], left.shape[1]:(left.shape[1]+right.shape[1]), :] = right
 		viz[right.shape[0]:(right.shape[0]+disp.shape[0]), :, :] = 255*disparity_to_color(disp)
-		viz[(right.shape[0]+disp.shape[0]):, :, :] = 255*disparity_to_color(disp_calculated)
-		cv2.imshow("viz.jpg", viz)
-		#
+		d2c = 255*disparity_to_color(disp_calculated)
+		viz[(right.shape[0]+disp.shape[0]):(right.shape[0]+disp.shape[0]+d2c.shape[0]), :, :] = d2c
+		viz[(right.shape[0]+disp.shape[0]+d2c.shape[0]):, :, :] = cm
+		cv2.imwrite(os.path.join(vizdir, name).replace(".png", ".jpg"), viz)
+
+	if show:
 		disp_calculated[ ~valid_mask ] = 0
 		cv2.imshow('disp (calculated, viewed in color)', disparity_to_color(disp_calculated))
 		cv2.imshow('error mask (green=OK, red=error, black=no data)', color_mask.permute(1, 2, 0).numpy())
@@ -157,9 +167,9 @@ def compute_kitti_result_for_image_pair(_calc_disparity, folder, name, show=True
 	else:
 		return ( outlier_mask.sum() / valid_mask.sum() ).item()
 
-def eval_kitti2015(model, folder):
+def eval_kitti2015(model, folder, filtering, vizdir):
 	def _calc_disparity(img0, img1):
-		return calc_disparity(model, img0, img1, filtering="median")
+		return calc_disparity(model, img0, img1, filtering=filtering)
 
 	nimages = 0
 	pctbadpts = 0
@@ -167,7 +177,7 @@ def eval_kitti2015(model, folder):
 	for root, dirs, filenames in os.walk(folder+'/image_2/'):
 		for filename in filenames:
 			if True:
-				p = compute_kitti_result_for_image_pair(_calc_disparity, folder, filename, show=False)
+				p = compute_kitti_result_for_image_pair(_calc_disparity, folder, filename, show=False, vizdir=vizdir, ignore_calc_zeros=filtering=="threshold")
 				if p is not None:
 					print("%s        |        %.1f" % (filename, 100*p))
 					nimages = nimages + 1
@@ -214,11 +224,13 @@ def parse_args():
 	parser.add_argument('modeldef', type=str, help='a script that defines the segmentation network')
 	parser.add_argument('--loadpath', type=str, default=None, help='path from which to load pretrained weights')
 	parser.add_argument('--kittipath', type=str, default="datasets/kitti2015/data_scene_flow/training/", help='path to KITTI data')
+	parser.add_argument('--filtering', type=str, default="median", help='filtering applied to the raw disparity map')
+	parser.add_argument('--vizdir', type=str, default=None, help='directory where to store visualizations')
 
 	return parser.parse_args()
 
 if __name__ == "__main__":
 	args = parse_args()
 	model = make_model(args.modeldef, args.loadpath)
-	p = eval_kitti2015(model, args.kittipath)
+	p = eval_kitti2015(model, args.kittipath, args.filtering, args.vizdir)
 	print("* bad points: %.2f%%" % p)
