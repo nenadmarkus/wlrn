43d0353dc7c302ee0507bfc92c541d76e5d5c06d
Nenad <nenad@m1mac>
2023-02-06 15:54:16 +0100

    mccnn_smaller

--- selfsup-depth/models/mccnn_smaller.py
+++ selfsup-depth/models/mccnn_smaller.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 
 #
 class McCNN(nn.Module):
-    def __init__(self, **config):
+    def __init__(self, config):
         super(McCNN, self).__init__()
         self.features = config.get('features', 64)
         self.ksize = config.get('ksize', 3)
         self.padding = config.get('padding', 1)
+        self.inpchn = config.get('inpchn', 1)
         self.unaries = nn.Sequential(
-            nn.Conv2d(1, self.features, self.ksize, padding=self.padding),
+            nn.Conv2d(self.inpchn, self.features, self.ksize, padding=self.padding),
             nn.ReLU(inplace=True),
             nn.Conv2d(self.features, self.features, self.ksize, padding=self.padding)
         )
@@ -20,4 +21,9 @@ class McCNN(nn.Module):
 #
 def init():
 	#
-	return McCNN()
+	return McCNN({
+	    "inpchn": c,
+	    "features": 64,
+	    "ksize": 3,
+	    "padding": 1
+	})
